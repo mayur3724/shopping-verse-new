91e9a3cf94e5041a48ce917dfd35c067482d0637
mayurpagare <mayurpagare992@gmail.com>
2024-03-21 02:13:49 +0530

    contoller

---
+++ shopping-verse-new/src/main/java/com/example/shoppingversenew/service/CustomerService.java
@@ -0,0 +1,13 @@
+package com.example.shoppingversenew.service;
+
+import com.example.shoppingversenew.dto.request.CustomerRequestDto;
+import com.example.shoppingversenew.dto.response.CustomerResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    public CustomerResponseDto addCustomer(CustomerRequestDto customerRequestDto) {
+
+    }
+}
